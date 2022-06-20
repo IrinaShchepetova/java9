9c9c7207f0588e017d3775d72eef6fd2baa340ca
Irina <sepetovairina@gmail.com>
2022-06-20 18:13:44 +0300

    HW9

--- src/java9/Main.java
+++ src/java9/Main.java
@@ -1,8 +1,33 @@
 package java9;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Student> students = HW.getStudents();
+        Course course = HW.getRandomCourse();
+        System.out.println(getUnicCourse(students));
+        System.out.println(inquisitiveStudents(students));
+        System.out.println(getStudentsCourse(students, course));
+    }
+    public static List<Course> getUnicCourse(List<Student> students){
+        return students.stream()
+                .flatMap(student -> student.getCourses().stream())
+                .distinct()
+                .collect(Collectors.toList());
+    }
+    public static List<Student> getStudentsCourse(List<Student> students, Course course){
+        System.out.println("Студенты, проходящие курс "+course);
+        return students.stream()
+                .filter(student -> student.getCourses().contains(course))
+                .collect(Collectors.toList());
+    }
+    public static List<Student> inquisitiveStudents (List<Student> students) {
+        return students.stream()
+                .sorted((s1, s2) -> (s2.getCourses().size()) - (s1.getCourses().size()))
+                .limit(3)
+                .collect(Collectors.toList());
     }
 }
